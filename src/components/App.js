f9ce2888e836dd6221b96abac2e7205fe4cbfcc9
laurageeme <registro.laura@gmail.com>
2020-05-15 21:20:42 +0200

    build main structure

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 /* import logo from './logo.svg'; */
-import './App.scss';
+import '../stylesheets/App.scss';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
 {/*         <img src={logo} className="App-logo" alt="logo" /> */}
-    hola
+    hola puedo escribir
       </header>
     </div>
   );
