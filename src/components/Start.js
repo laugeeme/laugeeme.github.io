0dcd2ccfef1b3f95e4de70944cb782fe104c1884
laurageeme <registro.laura@gmail.com>
2020-05-16 19:35:00 +0200

    add some styles to start component

--- src/components/Start.js
+++ src/components/Start.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import lauraPhoto from '../assets/images/laura1.png'
 
 
 
 function Start() {
     return (
       <div className="start-container scroll-area">
-          este es el start container
+          <div className="lauraPhoto-container">
+            <img src={lauraPhoto} alt="Laura"></img>
+          </div>
+          <div className="lauraText-container">
+          Welcome... Let's get started.
+          </div>
 
       </div>
     );
