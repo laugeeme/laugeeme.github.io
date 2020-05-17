2fbd5abe1aa2d45ef4e3b1d2b8166b53a9865204
laugeeme <registro.laura@gmail.com>
2020-05-17 16:17:24 +0200

    add basic json with list of projects

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,23 @@ import React from 'react';
 function About() {
   return (
     <section>
-      <div className="about-container scroll-area">este es el about container</div>
+      <div className="about-container scroll-area">
+        <div className="about-container_text">
+          <h2 className="about-container_title">{'{' + 'About' + '}'}</h2>
+          <p>
+            My name is Laura García. I'm an Agricultural Engineer who, being
+            passionate about the world of technology, has decided to reinvent
+            herself as a Front-End programmer to develop all my skills in this
+            field.
+          </p>
+        </div>
+
+        <div className="about-container_skills">
+          <h2 className="about-container_skills__title">
+            {'{' + 'Technical skills' + '}'}
+          </h2>
+        </div>
+      </div>
     </section>
   );
 }
